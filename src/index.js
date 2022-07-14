1a297207d764d8f9a9617b1eeb307b708bbe33f5
Akbar <md.akbarahmed786@gmail.com>
2022-07-15 02:32:18 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -27,8 +27,8 @@ app.get("/", (req, res) => {
 });
 
 ///Available Routes
-app.use("/api/auth", require("./routes/auth"));
-app.use("/api/notes", require("./routes/notes"));
+// app.use("/api/auth", require("./routes/auth"));
+// app.use("/api/notes", require("./routes/notes"));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
